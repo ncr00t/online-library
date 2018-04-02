c5c8b16d04621ad4030c80ff9bc4f642c13bf77c
ncr00t <geritix@mail.ru>
2018-04-03 02:20:18 +0300

    change return boolean value in method deleteBookById on int book id

--- src/main/java/com/onlineLibrary/library/dao/interfaces/BookDAO.java
+++ src/main/java/com/onlineLibrary/library/dao/interfaces/BookDAO.java
@@ -14,6 +14,6 @@ public interface BookDAO {
     List<Book> getBooksByName(String bookName);
     List<Book> getBooksByGenre(Genre genre);
     List<Book> getBooksByPublisher(Publisher publisher);
-    boolean deleteBookById(int bookId);
+    int deleteBookById(int bookId);
     Object getFieldValueById(int id, String fieldName);
 }
