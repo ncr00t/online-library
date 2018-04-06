6fc4216284ba93766c97a70e99ddf9870ec3c626
ncr00t <geritix@mail.ru>
2018-04-07 02:48:39 +0300

    implemented servlet for show book cover on main page

---
+++ src/main/java/com/onlineLibrary/library/book_cover_servlet/ViewBookCover.java
@@ -0,0 +1,41 @@
+package com.onlineLibrary.library.book_cover_servlet;
+
+import com.onlineLibrary.library.service.LibraryService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@WebServlet(name = "ViewBookCover",
+urlPatterns = {"/ViewBookCover"})
+public class ViewBookCover extends HttpServlet {
+
+    protected  void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("image/jpeg");
+
+        try (OutputStream outputStream = response.getOutputStream()){
+            int bookCoverId = Integer.valueOf(request.getParameter("bookCoverId"));
+            LibraryService libraryService = (LibraryService) getServletContext().getAttribute("libraryService");
+            byte[] bookCover = libraryService.getBooks().get(bookCoverId).getImage();
+            response.setContentLength(bookCover.length);
+            outputStream.write(bookCover);
+        }catch (Exception ex){
+            System.out.println("No load book cover.");
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
