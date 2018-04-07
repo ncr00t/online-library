989494b986ccb94560765c8873a8b84948436533
ncr00t <geritix@mail.ru>
2018-04-07 21:41:09 +0300

    added encoding filter for jsf pages

---
+++ src/main/java/com/onlineLibrary/library/servlets/encoding_filter_for_jsf_pages/EncodingFilter.java
@@ -0,0 +1,26 @@
+package com.onlineLibrary.library.servlets.encoding_filter_for_jsf_pages;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter  implements Filter {
+
+    private String encoding = "utf-8";
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        request.setCharacterEncoding(encoding);
+        response.setCharacterEncoding(encoding);
+        filterChain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String encodingParam = filterConfig.getInitParameter("encoding");
+        if (encodingParam != null) {
+            encoding = encodingParam;
+        }
+    }
+
+    public void destroy() {
+
+    }
+}
